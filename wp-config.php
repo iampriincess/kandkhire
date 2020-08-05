1080d9646d0185143bf8692a4b5b79907b3a29c6
unknown <princessmaebuangjug@gmail.com>
2020-08-05 22:04:34 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,20 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'kandhire_db' );
+// define( 'DB_NAME', 'kandhire_db' );
+define( 'DB_NAME', 'dfqpnalknrocmb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+// define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wrwuofmsohrjmj' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+// define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '3c943218b3ea2e2e848ea98ed6894d02d909c22332abdec2f50fbefbc2b28525' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+// define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'ec2-34-238-26-109.compute-1.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
